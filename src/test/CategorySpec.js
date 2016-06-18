2448a69f17fe5f77c60f1cc851cf93e724d96dd6
Victor <victuxbb@gmail.com>
2016-06-18 11:23:20 +0200

    Added mocha for testing use cases with Observables

--- src/test/CategorySpec.js
+++ src/test/CategorySpec.js
@@ -1,14 +1,46 @@
-import {expect} from 'chai';
-import openMarket from 'src/openMarket';
-import sinon from 'sinon';
+import openMarket from '../openMarket';
+import la from 'lazy-ass';
+import is from 'check-more-types';
+import Rx from 'rx';
+import FixturesService from '../openMarket/product_catalog/infrastructure/service/FixturesService';
+/**
+ * Howto
+ * https://glebbahmutov.com/blog/testing-reactive-code/
+ */
 
 describe("Test framework", function() {
 
+    this.fixturesService = new FixturesService();
+    this.fixturesService.load();
+
+    const observableCategories = openMarket.get("categories_list_all_use_case")
+        .findAll()
+        .flatMap(arrayData => Rx.Observable.from(arrayData))
+        ;
+    const noop = () => {}
+
     it("should return an Observable of campaigns", function() {
-        openMarket.get("categories_list_all_use_case")
-            .findAll()
-            .subscribe(category => {
-                expect(category.id).toEqual(jasmine.any(String));
-            });
+        la(is.fn(observableCategories.subscribe),'has subscribe method')
     });
+
+    it('should finish well', (done) => {
+        observableCategories.subscribe(noop, noop, done)
+    })
+
+    it("should return 12 campaigns", (done) => {
+        var count = 0
+        const onNumber = () => { count += 1 }
+        observableCategories.subscribe(onNumber, noop, () => {
+            la(count === 12, 'got '+ count + ' campaigns')
+            done()
+        })
+
+    });
+    it("has no errors", (done) => {
+        Rx.config.longStackSupport = true
+        const crash = (err) => { throw err }  // rethrow
+        observableCategories.subscribe(noop, crash, done)
+    })
+
+
 });
