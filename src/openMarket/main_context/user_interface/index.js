5edd1708256da5a0f2d5d8ca4edf76e819aa67df
Victor <victuxbb@gmail.com>
2017-02-02 22:02:21 +0100

    Redux & React Added redux and react dependencies Added examples based on https://github.com/chentsulin/electron-react-boilerplate

--- src/openMarket/main_context/user_interface/index.js
+++ src/openMarket/main_context/user_interface/index.js
@@ -1,31 +1,17 @@
-import Cycle from '@cycle/rx-run';
-import {div, label, input, hr, h1, img, p, makeDOMDriver} from '@cycle/dom';
-import makeOpenMarketDriver from '../infrastructure/cyclejs/OpenMarketDriver';
-import {html} from 'snabbdom-jsx';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import routes from './routes';
+import configureStore from './store/configureStore';
 
+const store = configureStore();
+const history = syncHistoryWithStore(hashHistory, store);
 
-function main(sources) {
-    const categoriesListAllUseCase$ = sources.OPENMARKET.select("categories_list_all_use_case");
-
-    return {
-        DOM: categoriesListAllUseCase$
-            .flatMap(categoriesListAllUseCase => categoriesListAllUseCase.findAll())
-            .map(categories =>
-                <div>
-                    {
-                        categories.map(category =>
-                        <div>
-                            <p>{category.name}</p>
-                            <img src={category.imageUrl} with="100" height="100"></img>
-                        </div>
-                        )
-                    }
-                </div>
-            )
-    };
-}
-
-Cycle.run(main, {
-    DOM: makeDOMDriver('#main-container'),
-    OPENMARKET: makeOpenMarketDriver()
-});
+render(
+    <Provider store={store}>
+        <Router history={history} routes={routes} />
+    </Provider>,
+    document.getElementById('main-container')
+);
