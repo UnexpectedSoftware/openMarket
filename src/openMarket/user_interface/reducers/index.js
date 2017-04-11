0c2daaa4230c07efd803a88f32c124e48ff2d392
Victor <victuxbb@gmail.com>
2017-04-11 19:44:00 +0200

    Feature/ui add product (#7)
    * Redux form create product
    
    * Add poc of product creation, pending connect form state with global state
    
    * Redux form
    * Add example form
    
    * Prove of concept
    * Add product reducer
    * Add product action
    * Handle form state in product create form
    
    * Migration to RxJS v5
    
    * Fix all domain test with new RxJS version
    
    * Redux-Observable
    
    * Add poc using epics to orchestrate actions
    * Modify store configuration for dev and pro
    * Add poc of loading categories on product form through a componentWillMount dispatching an action
    
    * poc of using epics to create a new product and handle redux form state
    
    * Refactoring to components concept layer
    
    * Add all product related concepts to component folder
    * Rename all epics, reducers, actions and react components
    
    ** Thanks to @Arkioner for helping me to become a little more abstract crazy
    
    * Add poc of redux form validations
    
    * Try poc of sync redux form validations
    
    * Field validations
    
    * Add inline field validations
    * Add general lib validations to be used in all forms

--- src/openMarket/user_interface/reducers/index.js
+++ src/openMarket/user_interface/reducers/index.js
@@ -1,11 +1,18 @@
 // @flow
 import { combineReducers } from 'redux';
-import { routerReducer as routing } from 'react-router-redux';
-import counter from './counter';
 
-const rootReducer = combineReducers({
+import { routerReducer as routing } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form'
+import counter from './counter';
+import newProductReducer from '../product/new_product/reducer';
+
+
+
+export const rootReducer = combineReducers({
   counter,
+  newProduct: newProductReducer,
+  form: formReducer,
   routing
 });
 
-export default rootReducer;
+
