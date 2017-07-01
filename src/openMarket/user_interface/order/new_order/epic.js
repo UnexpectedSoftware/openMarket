d972c55e64b1a96d4923940d28a4e5c643237bef
Victor <victuxbb@gmail.com>
2017-07-02 00:26:51 +0200

    Delete barcode field after Enter key pressed

--- src/openMarket/user_interface/order/new_order/epic.js
+++ src/openMarket/user_interface/order/new_order/epic.js
@@ -1,8 +1,10 @@
 import * as newOrderActions from "./action";
 import OpenMarket from "../../../index";
+import * as Rx from "rxjs";
+import {reset} from 'redux-form';
 
 export default action$ =>
   action$.ofType(newOrderActions.NEW_ORDER_PRODUCT_FETCH)
     .flatMap(action => OpenMarket.get("products_find_use_case").findProductByBarcode({barcode: action.barcode}))
-    .do(data => console.log("product:",data))
-    .map(product => newOrderActions.newOrderProductFetched(product));
+    .map(product => newOrderActions.newOrderProductFetched(product))
+    .mergeMap(action => Rx.Observable.of(reset('new_order'),action));
