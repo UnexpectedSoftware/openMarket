a5916e5ee88b462be7087dcd2eb370135077f563
Victor <victuxbb@gmail.com>
2017-08-22 16:46:27 +0200

    Weighted product * Add weighted field to product domain * Remove imageUrl field from product domain, unused for now

--- src/openMarket/user_interface/product/new_product/ReduxForm.js
+++ src/openMarket/user_interface/product/new_product/ReduxForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { required, maxLength15, number, minValue1 } from '../../validations/formValidations';
+import { required, maxLength15, number, greaterThan0 } from '../../validations/formValidations';
 
 const renderInput = field => (
   <div>
@@ -22,11 +22,11 @@ class ReduxForm extends Component {
         <Field name="barcode" component={renderInput} type="text" placeholder="Barcode" validate={[required, maxLength15]}/>
         <Field name="name" component={renderInput} type="text" placeholder="Name" validate={required}/>
         <Field name="description" component={renderInput} type="textarea" placeholder="Description"/>
-        <Field name="price" component={renderInput} type="text" placeholder="Price" validate={[required, minValue1, number]}/>
-        <Field name="basePrice" component={renderInput} type="text" placeholder="Base Price" validate={[required, minValue1, number]}/>
-        <Field name="stock" component={renderInput} type="text" placeholder="Stock" validate={[required, minValue1, number]}/>
-        <Field name="stockMin" component={renderInput} type="text" placeholder="Stock Minimum" validate={[required, minValue1, number]}/>
-        <Field name="imageUrl" component={renderInput} type="text" placeholder="Image url"/>
+        <Field name="price" component={renderInput} type="text" placeholder="Price" validate={[required, greaterThan0, number]}/>
+        <Field name="basePrice" component={renderInput} type="text" placeholder="Base Price" validate={[required, greaterThan0, number]}/>
+        <Field name="stock" component={renderInput} type="text" placeholder="Stock" validate={[required, greaterThan0, number]}/>
+        <Field name="stockMin" component={renderInput} type="text" placeholder="Stock Minimum" validate={[required, greaterThan0, number]}/>
+        <Field name="weighted" component={renderInput} type="checkbox" placeholder="Weighted"/>
         <div>
           <label htmlFor="categoryId">Category</label>
           <Field name="categoryId" component="select">
