162adcbc499a50c2957577f846b4e579a038db6e
Victor <victuxbb@gmail.com>
2017-10-04 17:22:54 +0200

    Add menu state handler inside react container, simple solution for now, maybe later we should bind the state to redux, currently if you reload the app you break the menu state but in production is not posible to reload a page

--- src/openMarket/user_interface/containers/Navbar.js
+++ src/openMarket/user_interface/containers/Navbar.js
@@ -3,30 +3,45 @@ import {Link} from "react-router";
 import Menu, {SubMenu, MenuItem} from 'rc-menu';
 
 export default class Navbar extends Component {
+
+  constructor(props,context) {
+    super(props, context);
+    this.state =  {
+      openKeys: ['1-1'],
+    };
+  }
+
+  onOpenChange(openKeys) {
+    console.log('onOpenChange', openKeys);
+    this.setState({
+      openKeys,
+    });
+  }
+
+
   render() {
     return (
       <div>
         <div className={"header"}>
           <h1>OpenMarket</h1>
-          <Menu mode="horizontal">
-            <MenuItem><Link to="/">Dashboard</Link></MenuItem>
-            <SubMenu title="Products">
-              <MenuItem>
-                <Link to="/create_product">new Product!</Link>
-                <Link to="/edit_product">edit Product!</Link>
-                <Link to="/list_products_low_stock">List Products with LOW stock!</Link>
-              </MenuItem>
+          <Menu
+            mode="horizontal"
+            onOpenChange={this.onOpenChange.bind(this)}
+            openKeys={this.state.openKeys}
+            defaultSelectedKeys={this.state.openKeys}
+          >
+            <MenuItem key="1-1"><Link to="/">Dashboard</Link></MenuItem>
+            <SubMenu key="2" title="Products">
+              <MenuItem key="2-1"><Link to="/create_product">new Product!</Link></MenuItem>
+              <MenuItem key="2-2"><Link to="/edit_product">edit Product!</Link></MenuItem>
+              <MenuItem key="2-3"><Link to="/list_products_low_stock">List Products with LOW stock!</Link></MenuItem>
             </SubMenu>
-            <SubMenu title="Categories">
-              <MenuItem>
-                <Link to="/create_category">new Category!</Link>
-              </MenuItem>
+            <SubMenu key="3" title="Categories">
+              <MenuItem key="3-1"><Link to="/create_category">new Category!</Link></MenuItem>
             </SubMenu>
-            <SubMenu title="Orders">
-              <MenuItem>
-                <Link to="/create_order">new Order!</Link>
-                <Link to="/list_orders">List Orders!</Link>
-              </MenuItem>
+            <SubMenu key="4" title="Orders">
+              <MenuItem key="4-1"><Link to="/create_order">new Order!</Link></MenuItem>
+              <MenuItem key="4-2"><Link to="/list_orders">List Orders!</Link></MenuItem>
             </SubMenu>
           </Menu>
         </div>
