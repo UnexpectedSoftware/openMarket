48295c468c7ad4ccc54a17ce4334399834d3c2aa
Victor <victuxbb@gmail.com>
2017-10-08 20:18:22 +0200

    Remove console log

--- src/openMarket/user_interface/home/epicFactory.js
+++ src/openMarket/user_interface/home/epicFactory.js
@@ -7,9 +7,6 @@ const endDate = moment();
 /*const startDate = moment('2004-12-31 00:00:00','YYYY-MM-DD HH:mm:ss');
 const endDate = moment('2005-01-06 23:59:59','YYYY-MM-DD HH:mm:ss');*/
 
-console.log(startDate.isValid());
-console.log(endDate.isValid());
-
 export const makeHomePageLoadedEpic = ordersStatisticsUseCase => action$ =>
   action$
     .filter(action => action.type === homeActions.HOME_PAGE_LOADED)
