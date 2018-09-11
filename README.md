401977ed3f7fdcdd9012c07f60231d2518098923
Victor <victuxbb@gmail.com>
2018-09-11 02:05:34 +0200

    Fix initial merge with boilerplate project

---
+++ README.md
@@ -0,0 +1,59 @@
+# OpenMarket
+Desktop application to administrate a generic market with stocks, products, categories etc...
+
+## Requirements
+
+To clone and run this repository you'll need Git and Node.js (which comes with npm) installed on your computer. From your command line:
+
+```bash
+    git clone https://github.com/UnexpectedSoftware/openMarket.git
+    cd openMarket
+    npm install
+```
+
+## Getting started
+
+To start the application:
+
+```bash
+    npm run dev
+```
+
+## Packaging
+
+To package apps for the local platform:
+
+```bash
+$ npm run package
+```
+
+To package apps for all platforms:
+
+First, refer to [Multi Platform Build](https://github.com/electron-userland/electron-builder/wiki/Multi-Platform-Build) for dependencies.
+
+Then,
+```bash
+$ npm run package-all
+```
+
+To package apps with options:
+
+```bash
+$ npm run package -- --[option]
+```
+
+## Further commands
+
+To run the application without packaging run
+
+```bash
+$ npm run build
+$ npm start
+```
+
+To run End-to-End Test
+
+```bash
+$ npm run build
+$ npm run test-e2e
+```
---
+++ src/test/CategorySpec.js
@@ -0,0 +1,185 @@
+import openMarket from '../openMarket';
+import la from 'lazy-ass';
+import is from 'check-more-types';
+import Rx from 'rx';
+import FixturesService from '../openMarket/infrastructure/service/FixturesService';
+/**
+ * Howto
+ * https://glebbahmutov.com/blog/testing-reactive-code/
+ */
+
+/**
+ * @type {Observable.<Array.<Category>>}
+ */
+const observableCategories = openMarket.get("categories_list_all_use_case");
+/**
+ * @type {CreateCategory}
+ */
+const observableCreateCategory = openMarket.get("categories_create_use_case");
+/**
+ * @type {UpdateCategory}
+ */
+const observableUpdateCategory = openMarket.get("categories_update_use_case");
+/**
+ *
+ * @type {FindCategoryById}
+ */
+const observableFindByIdCategory = openMarket.get("categories_find_by_id_use_case");
+
+
+const noop = () => {}
+const crash = (err) => { throw err }  // rethrow
+Rx.config.longStackSupport = true
+
+describe("Category find by id use case", function(){
+
+    beforeEach(function() {
+        this.fixturesService = new FixturesService();
+        this.fixturesService.load();
+    });
+
+    it("should return an Observable with no elements",(done) =>{
+        var count = 0
+        const onNumber = () => { count += 1 }
+        observableFindByIdCategory
+            .findById({
+                id: "non-existent"
+            })
+            .subscribe(onNumber,noop,() =>{
+                la(count === 0, 'got '+ count + ' campaigns')
+                done()
+            });
+    });
+    it("has no errors and complete", (done) => {
+
+        observableFindByIdCategory
+            .findById({
+                id: "non-existent"
+            })
+            .subscribe(noop, crash, done)
+    })
+
+});
+
+
+describe("Category list all use case", function() {
+
+    beforeEach(function() {
+        this.fixturesService = new FixturesService();
+        this.fixturesService.load();
+    });
+
+    it("should return an Observable of campaigns", function() {
+        la(is.fn(observableCategories.findAll().subscribe),'has subscribe method')
+    });
+
+    it('should finish well', (done) => {
+        observableCategories.findAll().subscribe(noop, noop, done)
+    })
+
+    it("should return 12 campaigns", (done) => {
+        var count = 0
+        const onNumber = () => { count += 1 }
+        observableCategories
+            .findAll()
+            .flatMap(arrayData => Rx.Observable.from(arrayData))
+            .subscribe(onNumber, noop, () => {
+            la(count === 12, 'got '+ count + ' campaigns')
+            done()
+        })
+
+    });
+
+    it("has no errors and complete", (done) => {
+
+        observableCategories
+            .findAll()
+            .flatMap(arrayData => Rx.Observable.from(arrayData))
+            .subscribe(noop, crash, done)
+    })
+
+
+});
+
+describe("Category create use case", function() {
+
+    beforeEach(function() {
+        this.fixturesService = new FixturesService();
+        this.fixturesService.load();
+    });
+
+    it("should create a new campaign and then would be 13 campaigns", (done) =>{
+        var count = 0
+        const onNumber = () => { count += 1 }
+
+        observableCreateCategory.createCategory({
+            name: "category test",
+            imageUrl: "http://www.google.es/caca"
+        })
+            .flatMap(observableCategories.findAll())
+            .flatMap(arrayData => Rx.Observable.from(arrayData))
+            .subscribe(onNumber, noop, () => {
+                la(count === 13, 'got '+ count + ' campaigns')
+                done()
+            });
+    });
+
+    it("has no errors and complete", (done) => {
+        observableCreateCategory.createCategory({
+            name: "category test",
+            imageUrl: "http://www.google.es/caca"
+        }).subscribe(noop, crash, done);
+    })
+
+});
+
+describe("Category update use case", function(){
+
+    beforeEach(function() {
+        this.fixturesService = new FixturesService();
+        this.fixturesService.load();
+    });
+
+    it("should update the first campaign with a new name and image url", (done) =>{
+        observableCategories.findAll()
+            .flatMap(arrayData => Rx.Observable.from(arrayData))
+            .first()
+            .flatMap(firstCategory => {
+                return observableUpdateCategory.updateCategory({
+                    id: firstCategory.id,
+                    name: "pepe",
+                    imageUrl: "http://42.com"
+                })
+            })
+            .subscribe(done,crash,noop);
+    });
+
+    it("has no errors and complete", (done) => {
+        observableCategories.findAll()
+            .flatMap(arrayData => Rx.Observable.from(arrayData))
+            .first()
+            .flatMap(firstCategory => {
+                return observableUpdateCategory.updateCategory({
+                    id: firstCategory.id,
+                    name: "pepe",
+                    imageUrl: "http://42.com"
+                })
+            })
+            .subscribe(noop,crash,done);
+    })
+
+
+    it("should try to update a non existent campaign and return error", (done) =>{
+        const crash = (err) => {
+            la(is.error(err),'has error')
+            done()
+        }
+        observableUpdateCategory.updateCategory({
+            id: "non-existent",
+            name: "pepe",
+            imageUrl: "http://42.com"
+        }).subscribe(noop,crash,noop);
+    });
+});
+
+
